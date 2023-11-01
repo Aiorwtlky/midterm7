a119a84e34bce96de76f8ca71e9a514af7790807
Aiorwtlky <wenvan33@icloud.com>
2023-11-01 15:04:54 +0800

    frogggggg

--- mysite/admin.py
+++ mysite/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from mysite.models import Post
+admin.site.register(Post)
 
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'pub_date')
+
